7e672bd1df949af4b7bcca54851365a9517756dd
Benjamin Roberts <benjamin.roberts01@mymail.champlain.edu>
2020-03-15 20:25:59 -0400

    Finished Linked Lists Cont.

--- In-Class Lecture/Linked List Cont/Linked List Cont/linkedlist.cpp
+++ In-Class Lecture/Linked List Cont/Linked List Cont/linkedlist.cpp
@@ -1,22 +1,12 @@
-/**
- * @file linkedlist.cpp
- * @brief  This is a lecture program that illustrates the definiition of a Linked List class and topics from Gaddis 17.
- *
- * @author Bilbo B. Baggins
- * @credits Gaddis Chapter 17
- * @assignment Lecture 8.1
- * @date 3/2/2020
- *
- */#include "linkedlist.h"
+#include "linkedlist.h"
 #include <iostream>
-#include <fstream>
 
-using namespace bilbo;
+using namespace benSpace;
 
 //2.1
 void List::push_back(int number){
     //does the list have an item?
-    if(head){
+    if(head) {
         Node* seeker = head;
         while(seeker->next) {
             seeker=seeker->next;
@@ -24,8 +14,7 @@ void List::push_back(int number){
         //we should be at the end of the list
         seeker->next = new Node(number);
     }
-    else
-    {
+    else {
         //empty
         head = new Node(number);
     }
@@ -33,34 +22,71 @@ void List::push_back(int number){
 //3.1
 void List::walk() const {
     Node* seeker = head;
-    while(seeker)
-    {
+    while(seeker){
         std::cout << seeker->data << " [" << seeker << "]" << std::endl;
         seeker = seeker->next;
     }
 }
 //4.1 List::~List()
-List::~List()
-{
+List::~List() {
     Node* seeker = head;
-    while(seeker)
-    {
+    while(seeker) {
         Node* collector = seeker;
         seeker=seeker->next;
         //now that we've moved on to the next Node
         //we can delete the one we just bypassed.
         std::cout << collector->data << "~" << collector << "~" << std::endl;
         delete collector;
-        
     }
     head = nullptr;
 }
 //5.1  void List::push(int number)
-void List::push(int number)
-{
+void List::push(int number) {
     Node* newNode = new Node(number, head);
     head = newNode;
 }
-//6  int List::size() const
-//7  void List::remove(int number)
-//8  int List::load(std::string fileName)
+
+//6
+int List::size() const{
+    int len = 0;
+    Node* seeker = head;
+    while(seeker){
+        len++;
+        seeker=seeker->next;
+    }
+    return len;
+}
+//7
+void List::remove(int number){
+    Node* seeker = head;
+    Node* pSeeker = seeker;
+    while(seeker) {
+        if(seeker->data == number){
+            std::cout << seeker->data << "-" << seeker << "-" << std::endl;
+            pSeeker->next = seeker->next;
+            delete seeker;
+        }
+        pSeeker = seeker;
+        seeker = seeker->next;
+    }
+}
+//8
+int List::load(std::string fileName){
+    int added = 0;
+    std::string readNum = "";
+    //std::ifstream input(fileName, std::ios::in);
+    std::ifstream input(fileName);
+    
+    if (input.good()){
+        while (!input.eof()){
+            getline(input, readNum);
+            if (readNum != ""){
+                push_back(stoi(readNum));
+                added++;
+            }
+        }
+        
+        input.close();
+    }
+    return added;
+}
--- In-Class Lecture/Linked List Cont/Linked List Cont/linkedlist.h
+++ In-Class Lecture/Linked List Cont/Linked List Cont/linkedlist.h
@@ -1,28 +1,16 @@
-/**
- * @file linkedlist.h
- * @brief  This is a lecture program that illustrates the declaration of a Linked List class
- * and topics from Gaddis 17.
- *
- * @author Bilbo B. Baggins
- * @credits Gaddis Chapter 17
- * @assignment Lecture 8.1
- * @date 3/2/2020
- *
- */
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
 #include <string>
+#include <fstream>
 
-namespace bilbo //pick your own
+namespace benSpace //pick your own
     {
     /** class List
      *  @brief a simple Linked List class used to illustrates topics from Gaddis 17
      */
-    class List
-    {
+    class List {
     protected:
-        struct Node
-        {
+        struct Node {
             int data;
             Node* next;
             //Remember a struct can have a constructor, just everything is public.
@@ -39,9 +27,12 @@ namespace bilbo //pick your own
         void walk() const;
         //5.
         void push(int number);
-        //6.  int size() const;
-        //7.  void remove(int number);
-        //8.  int load(std::string fileName);
+        //6.
+        int size() const;
+        //7.
+        void remove(int number);
+        //8.
+        int load(std::string fileName);
     }; //end List
     }//end name space
 #endif
